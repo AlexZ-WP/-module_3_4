b11e1bbe88843036c9046d4c8ae352dc51380b79
AlexZ-WP <alexz10@mail.ru>
2024-10-26 14:35:25 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -2,8 +2,8 @@ def single_root_words(root_word , *other_words):
     same_words = []
     root_word = root_word.lower()
     for i in other_words:
-        i = i.lower()
-        if (root_word in i) or (i in root_word):
+        #i = i.lower()
+        if (root_word.lower() in i) or (i.lower() in root_word):
             same_words.append(i)
 
     print(same_words)
